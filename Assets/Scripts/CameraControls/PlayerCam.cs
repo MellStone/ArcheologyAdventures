60ad6fc5a12b3671e05b416e84d82cd635537a32
MellStone <fly.ff531@gmail.com>
2023-04-20 10:53:53 +0200

    Transefer from old project
    Move scipt (old), scene, pickup obect & script

---
+++ Assets/Scripts/CameraControls/PlayerCam.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        orientation.rotation = Quaternion.Euler(0, 0, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+
+    }
+
+    private void Update()
+    {   // get mouse imput
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+
+
+}
+
+
