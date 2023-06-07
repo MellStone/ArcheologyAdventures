a9fb232343b3eaa54b35ecd005ae639ba9fbc0ca
MellStone <fly.ff531@gmail.com>
2023-06-07 11:34:55 +0200

    finish level1

--- Assets/EnemyHP.cs
+++ Assets/EnemyHP.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
 {
-
+    public Material deathMaterial;
+    private Renderer renderer;
     public int hp = 100;
     public int damage = 50;
+    private void Start()
+    {
+       renderer = GetComponent<Renderer>();
+    }
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other != null)
@@ -16,5 +28,15 @@ public class EnemyHP : MonoBehaviour
                 hp -= damage;          
             }
         }
+    }
+    private void Die()
+    {
+        renderer.material = deathMaterial;
+        StartCoroutine(WaitUnilDie());
+    }
+    IEnumerator WaitUnilDie()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Interaction/Button.cs
+++ Assets/Scripts/Interaction/Button.cs
@@ -5,36 +5,41 @@ public class Button : MonoBehaviour
 {
     [SerializeField] private GameObject[] objects;
     [SerializeField] private ParticleSystem particles;
-    [SerializeField][Range(0f, 20f)] private float timerCount;
+    [SerializeField] [Range(0f, 20f)] private float timerCount;
 
-    [SerializeField] private bool isPushed;
-
-
+    private bool isPushed = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other != null)
-        {
+        if (other != null && !isPushed)
+        {       
             isPushed = true;
             StopAllCoroutines();
             foreach (var obj in objects)
             {
-                obj.SetActive(false);
+                if(obj.activeSelf)
+                    obj.SetActive(false);
+                else 
+                    obj.SetActive(true);
             }
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        isPushed = false;
         StartCoroutine(Timer(timerCount));
     }
     private IEnumerator Timer(float timer)
     {
+        particles.gameObject.SetActive(true);
         particles.Play();
         yield return new WaitForSeconds(timer);
         foreach (var obj in objects)
         {
-            obj.SetActive(true);
+            if (obj.activeSelf)
+                obj.SetActive(false);
+            else
+                obj.SetActive(true);
         }
+        isPushed = false;
         particles.Stop();
     }
 }
